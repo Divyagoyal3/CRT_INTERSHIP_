0d077b88270f69603b70b6c02df4f4a532314508
Divyagoyal3 <Divyagoyal3>
2023-06-15 19:21:45 +0530

    music player done

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+    name :'I m mess',
+    path:'music/I m mess.mp3',
+    artist:'Bebe Rexha',
+    cover:'images/cover 1.png'
+},
+{
+    name :'On my way',
+    path:'music/On my way.mp3',
+    artist:'Alan Walker',
+    cover:'images/cover 2.png'
+},
+{
+    name :'Play',
+    path:'music/Play.mp3',
+    artist:'Alan Walker',
+    cover:'images/cover 3.png'
+},
+{
+    name :'We Donot talk Anymore',
+    path:'music/We Donot talk Anymore.mp3',
+    artist:'Justin Beber',
+    cover:'images/cover 4.png'
+},
+{
+    name :'Darkside',
+    path:'music/Darkside.mp3',
+    artist:'Alan Walker',
+    cover:'images/cover 5.png'
+},
+]
